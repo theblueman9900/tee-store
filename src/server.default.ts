7be9d28ebfa93914f5bec1c1ba56fb55bb7f9120
Souraj Pal <souraj.x.pal@godeskless.com>
2024-11-23 12:07:21 +0530

    Port changed

--- src/server.default.ts
+++ src/server.default.ts
@@ -15,7 +15,7 @@ import payload from 'payload'
 import { seed } from './payload/seed'
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 
 // Redirect root to the admin panel
 app.get('/', (_, res) => {
